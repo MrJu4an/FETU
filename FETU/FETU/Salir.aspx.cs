d71bc89b7828c1d2758c6f8ca057826e997fc2a2
Jorge Sabogal <55625968+MrJu4an@users.noreply.github.com>
2024-02-06 08:55:51 -0500

    Formulario de salir y ajuste de logo

---
+++ FETU/FETU/Salir.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using FETU.Querys;
+
+namespace FETU
+{
+    public partial class Salir : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                ClientScript.RegisterStartupScript(GetType(), "Script", "<script>removeLocalStorage('" + Session["CodUsuario"] + "')</script>");
+                QryLogin Delete = new QryLogin();
+                try
+                {
+                    Delete.insertGelogAud(Session["CodUsuario"].ToString(), Session["ModuloApp"].ToString() + "-SALIR", "SE CIERRA SESIÓN EN GOPETT ONLINE");
+                }
+                catch
+                {
+                    Session["CodUsuario"] = null;
+                    Session["NomUsuario"] = null;
+                    Session["FechaSistema"] = null;
+                    Session["ModuloApp"] = null;
+                    Session["TipoUsuario"] = null;
+                    Session["NombreTipo"] = null;
+                    Session["UsuEmail"] = null;
+                    Response.Redirect("Login.aspx", false);
+                }
+            }
+        }
+    }
+}
